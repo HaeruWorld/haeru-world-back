f6e08567ea2d92e984fd2309bc736d7d7e115138
손효재 <sonjw5128@gmail.com>
2022-12-15 02:59:09 +0900

    Initial commit

---
+++ src/main/java/com/haeru/haeruworldback/HaeruWorldBackApplication.java
@@ -0,0 +1,13 @@
+package com.haeru.haeruworldback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeruWorldBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaeruWorldBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haeru/haeruworldback/HaeruWorldBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haeru.haeruworldback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeruWorldBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
