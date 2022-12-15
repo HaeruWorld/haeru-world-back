7b29b0e937dcdddc2d6c4695c97d07cdaeeaa14b
손효재 <sonjw5128@gmail.com>
2022-12-15 21:16:05 +0900

    Feat : MarineCollection 로직 추가

---
+++ src/main/java/com/haeru/haeruworldback/domain/haeruplace/entity/MarineCollection.java
@@ -0,0 +1,34 @@
+package com.haeru.haeruworldback.domain.haeruplace.entity;
+
+import com.haeru.haeruworldback.domain.haeruplace.dto.MarineCollections;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MarineCollection {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long marineCollectionId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    public MarineCollections toDto() {
+        return MarineCollections.builder()
+                .name(this.name)
+                .imageUrl(this.imageUrl)
+                .build();
+    }
+
+}
