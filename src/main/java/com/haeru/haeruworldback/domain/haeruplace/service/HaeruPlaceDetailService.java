e4d35e8f69859eae3da164d2cfc94832a8a28399
손효재 <sonjw5128@gmail.com>
2023-03-29 23:39:00 +0900

    refactor: HaeruPlaceDetail 조회 로직 리팩토링

--- src/main/java/com/haeru/haeruworldback/domain/haeruplace/service/HaeruPlaceDetailService.java
+++ src/main/java/com/haeru/haeruworldback/domain/haeruplace/service/HaeruPlaceDetailService.java
@@ -1,11 +1,9 @@
 package com.haeru.haeruworldback.domain.haeruplace.service;
 
-import com.haeru.haeruworldback.domain.haeruplace.dto.HaeruPlaceDetailDto;
-import com.haeru.haeruworldback.domain.Location;
-import com.haeru.haeruworldback.domain.haeruplace.dto.MarineCollectionDto;
 import com.haeru.haeruworldback.domain.Area;
+import com.haeru.haeruworldback.domain.haeruplace.dto.HaeruPlaceDetailDto;
+import com.haeru.haeruworldback.domain.haeruplace.dto.MarineCollectionDto;
 import com.haeru.haeruworldback.domain.haeruplace.entity.HaeruPlace;
-import com.haeru.haeruworldback.domain.haeruplace.entity.MarineCollection;
 import com.haeru.haeruworldback.domain.haeruplace.exception.HaeruPlaceNotFoundException;
 import com.haeru.haeruworldback.domain.haeruplace.repository.HaeruPlaceRepository;
 import com.haeru.haeruworldback.domain.haeruplace.repository.MarineCollectionRepository;
@@ -14,6 +12,7 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,128 +33,88 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class HaeruPlaceDetailService {
 
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     private final HaeruPlaceRepository haeruPlaceRepository;
     private final MarineCollectionRepository marineCollectionRepository;
 
+    @Value("${khoaServiceKey}")
+    private String khoaServiceKey;
+
     // 해루질 장소 상세 조회
     public HaeruPlaceDetailDto getHaeruPlacesDetail(Long haeruPlaceId) {
-        HaeruPlaceDetailDto haeruPlaceDetail = new HaeruPlaceDetailDto();
-
         HaeruPlace findHaeruPlace = haeruPlaceRepository.findById(haeruPlaceId).orElseThrow(HaeruPlaceNotFoundException::new);
 
-        haeruPlaceDetail.setName(findHaeruPlace.getName());
-        haeruPlaceDetail.setAddress(findHaeruPlace.getAddress());
-
-        String marineCollectionNames = findHaeruPlace.getMarineCollections();
-
-        List<String> selectMarineCollections = Arrays.stream(marineCollectionNames.split(","))
+        List<MarineCollectionDto> marineCollectionDtos = Arrays.stream(findHaeruPlace.getMarineCollections().split(","))
+                .map(name -> marineCollectionRepository.findByName(name).toDto())
                 .collect(Collectors.toList());
+        List<String> canHaeruTime = canHaeruTimes(findHaeruPlace.getArea().toString());
 
-        List<MarineCollectionDto> list = new ArrayList<>();
-        for(String name : selectMarineCollections) {
-            MarineCollection findMarineCollection = marineCollectionRepository.findByName(name);
-            list.add(findMarineCollection.toDto());
-        }
-        haeruPlaceDetail.setMarineCollections(list);
-
-        Location location = new Location(findHaeruPlace.getLatitude(), findHaeruPlace.getLongitude());
-        haeruPlaceDetail.setLocation(location);
-
-        List<String> resultTime = getSeaTime(findHaeruPlace.getArea().toString());
-
-        if(resultTime.size() == 0) {
-            haeruPlaceDetail.setStartTime(null);
-            haeruPlaceDetail.setEndTime(null);
-        } else {
-            haeruPlaceDetail.setStartTime(resultTime.get(0));
-            haeruPlaceDetail.setEndTime(resultTime.get(1));
-        }
-
-        String areaName = findHaeruPlace.getArea().getAreaName();
-        haeruPlaceDetail.setAreaName(areaName);
-
-        return haeruPlaceDetail;
+        return findHaeruPlace.haeruPlaceDetailDto(marineCollectionDtos, canHaeruTime);
     }
 
+    // 해루질 가능 시간
+    public List<String> canHaeruTimes(String area) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime lawStartTime = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), 9, 0, 0);
+        LocalDateTime lawEndTime = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), 17, 0, 0);
 
-    public List<String> getSeaTime(String area) {
+        // 해당 지역의 저조 시간
+        LocalDateTime lowTideTime = getLowTideTime(lawStartTime, lawEndTime, areaLowTideTimes(area, now));
 
-        // 저조시간 관측 지역코드 추출
-        String areaCode = Area.valueOf(area).getAreaCode();;
+        // 법정 시간 비교
+        LocalDateTime startTime = lowTideTime.minusHours(1).isBefore(lawStartTime) ? lawStartTime : lowTideTime.minusHours(1);
+        LocalDateTime endTime = lowTideTime.plusHours(1).isAfter(lawEndTime) ? lawEndTime : lowTideTime.plusHours(1);
 
-        LocalDateTime localDateTime = LocalDateTime.now();
-        String date = localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        return List.of(mappingTimeFormat(startTime), mappingTimeFormat(endTime));
+    }
 
-        String reqURL = "https://www.khoa.go.kr/api/oceangrid/tideObsPreTab/search.do?ServiceKey=CA7svK0E39kQdfoWyx70rQ==&ObsCode="
-                + areaCode + "&Date=" + date + "&ResultType=json";
+    private String mappingTimeFormat(LocalDateTime time) {
+        return time.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+    private LocalDateTime getLowTideTime(LocalDateTime lawStartTime, LocalDateTime lawEndTime, List<String> tphTimes) {
+        return tphTimes.stream()
+                .map(time -> LocalDateTime.parse(time, DATE_TIME_FORMATTER))
+                .filter(time -> isBetweenLawTime(lawStartTime, lawEndTime, time))
+                .findFirst()
+                .orElseGet(null);
+    }
+
+    private boolean isBetweenLawTime(LocalDateTime lawStartTime, LocalDateTime lawEndTime, LocalDateTime dateTime) {
+        return (dateTime.minusHours(1).isAfter(lawStartTime) && dateTime.minusHours(1).isBefore(lawEndTime))
+                || (dateTime.plusHours(1).isAfter(lawStartTime) && dateTime.plusHours(1).isBefore(lawEndTime));
+    }
+
+    // 해당지역 저조 시간 구하기
+    private List<String> areaLowTideTimes(String area, LocalDateTime now) {
+        String areaCode = Area.valueOf(area).getAreaCode();
+        String nowDate = now.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+        String reqURL = "https://www.khoa.go.kr/api/oceangrid/tideObsPreTab/search.do?ServiceKey=" + khoaServiceKey + "&ObsCode="
+                + areaCode + "&Date=" + nowDate + "&ResultType=json";
         String result = "";
-        List<String> resultTimeList = new ArrayList<>();
-        try {
 
+        try {
             URL url = new URL(reqURL);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
-
             conn.setDoOutput(true);
             conn.setRequestProperty("charset", "UTF-8");
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String line;
-
             while ((line = br.readLine()) != null) {
                 result += line;
             }
-
-            // 하루 저조시간 리스트
-            List<String> timeList = getTphTime(result);
-
-            LocalDateTime now = LocalDateTime.now();
-            LocalDateTime lawStartTime = LocalDateTime.of(now.getYear(),now.getMonth(),now.getDayOfMonth(),9,0,0);
-            LocalDateTime lawEndTime = LocalDateTime.of(now.getYear(),now.getMonth(),now.getDayOfMonth(),17,0,0);
-
-            LocalDateTime resultTime = null;
-            for(int i = 0; i < timeList.size(); i++) {
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                LocalDateTime dateTime = LocalDateTime.parse(timeList.get(i), formatter);
-                if((dateTime.minusHours(1).isAfter(lawStartTime) && dateTime.minusHours(1).isBefore(lawEndTime))
-                || (dateTime.plusHours(1).isAfter(lawStartTime) && dateTime.plusHours(1).isBefore(lawEndTime))) {
-                    resultTime = dateTime;
-                    break;
-                }
-            }
-
-            if(resultTime == null) {
-                return resultTimeList;
-            }
-
-            // 법정 시간 비교
-            LocalDateTime startTime = resultTime.minusHours(1);
-            if (startTime.isBefore(lawStartTime)) startTime = lawStartTime;
-            LocalDateTime endTime = resultTime.plusHours(1);
-            if(endTime.isAfter(lawEndTime)) endTime = lawEndTime;
-
-
-            // 해루질 가능 시작, 종료시간
-            String startResultTime = startTime.format(DateTimeFormatter.ofPattern("HH:mm"));;
-            String endResultTime = endTime.format(DateTimeFormatter.ofPattern("HH:mm"));;
-
-            resultTimeList.add(startResultTime);
-            resultTimeList.add(endResultTime);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        return resultTimeList;
-    }
-
-    // 하루 저조 시간 구하기
-    public List<String> getTphTime(String result) {
-        List<String> list = new ArrayList<>();
+        List<String> lowTideTimes = new ArrayList<>();
         try {
             JSONParser parser = new JSONParser();
             JSONObject jsonObj = (JSONObject) parser.parse(result);
-
             JSONObject objResult = (JSONObject) jsonObj.get("result");
             JSONArray objData = (JSONArray) objResult.get("data");
 
@@ -164,16 +123,14 @@ public class HaeruPlaceDetailService {
                 String hl_code = (String) object.get("hl_code");
                 if (hl_code.equals("저조")) {
                     String time = (String) object.get("tph_time");
-                    list.add(time);
+                    lowTideTimes.add(time);
                 }
             }
-
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
-        return list;
+        return lowTideTimes;
     }
-
 
 }
