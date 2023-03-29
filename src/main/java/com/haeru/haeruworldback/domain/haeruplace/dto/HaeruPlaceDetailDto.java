25174f4395114de1c3026e0ee5a129bfc0d9acde
손효재 <sonjw5128@gmail.com>
2023-03-29 23:38:38 +0900

    refactor: DetailDto Builder 추가

--- src/main/java/com/haeru/haeruworldback/domain/haeruplace/dto/HaeruPlaceDetailDto.java
+++ src/main/java/com/haeru/haeruworldback/domain/haeruplace/dto/HaeruPlaceDetailDto.java
@@ -1,21 +1,17 @@
 package com.haeru.haeruworldback.domain.haeruplace.dto;
 
 import com.haeru.haeruworldback.domain.Location;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+
 import java.util.List;
 
 @Getter
-@Setter
-@RequiredArgsConstructor
-@AllArgsConstructor
+@Builder
 public class HaeruPlaceDetailDto {
 
     private String name;
     private String address;
-    private String areaName;
+    private String area;
     private Location location;
     private List<MarineCollectionDto> marineCollections;
     private String startTime;
--- src/main/java/com/haeru/haeruworldback/domain/haeruplace/entity/HaeruPlace.java
+++ src/main/java/com/haeru/haeruworldback/domain/haeruplace/entity/HaeruPlace.java
@@ -1,8 +1,11 @@
 package com.haeru.haeruworldback.domain.haeruplace.entity;
 
 import com.haeru.haeruworldback.domain.Area;
+import com.haeru.haeruworldback.domain.Location;
+import com.haeru.haeruworldback.domain.haeruplace.dto.HaeruPlaceDetailDto;
 import com.haeru.haeruworldback.domain.haeruplace.dto.HaeruPlaceDto;
 import com.haeru.haeruworldback.domain.MarkerPosition;
+import com.haeru.haeruworldback.domain.haeruplace.dto.MarineCollectionDto;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,32 +24,33 @@ import java.util.stream.Collectors;
 @NoArgsConstructor
 public class HaeruPlace {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long haeruPlaceId;
 
     @Column(nullable = false)
-    @ApiModelProperty(value="장소명", example = "도두어촌", required = true)
+    @ApiModelProperty(value = "장소명", example = "도두어촌", required = true)
     private String name;
 
     @Column(nullable = false)
-    @ApiModelProperty(value="주소", example = "제주시 사수포구 용담 2동 경계", required = true)
+    @ApiModelProperty(value = "주소", example = "제주시 사수포구 용담 2동 경계", required = true)
     private String address;
 
     @Column(nullable = false)
-    @ApiModelProperty(value="출현 어종", example = "보말,게,소라,미역", required = true)
+    @ApiModelProperty(value = "출현 어종", example = "보말,게,소라,미역", required = true)
     private String marineCollections;
 
     @Column(nullable = false)
     @Enumerated(value = EnumType.STRING)
-    @ApiModelProperty(value="지역", example = "JEJU", required = true)
+    @ApiModelProperty(value = "지역", example = "JEJU", required = true)
     private Area area;
 
     @Column(nullable = false)
-    @ApiModelProperty(value="위도", example = "33.511333", required = true)
+    @ApiModelProperty(value = "위도", example = "33.511333", required = true)
     private Double latitude;
 
     @Column(nullable = false)
-    @ApiModelProperty(value="경도", example = "126.483503", required = true)
+    @ApiModelProperty(value = "경도", example = "126.483503", required = true)
     private Double longitude;
 
     @Column(nullable = false)
@@ -63,6 +67,18 @@ public class HaeruPlace {
                 .markerPosition(new MarkerPosition(markerPositionX, markerPositionY))
                 .marineCollections(Arrays.stream(marineCollections.split(","))
                         .collect(Collectors.toList()))
+                .build();
+    }
+
+    public HaeruPlaceDetailDto haeruPlaceDetailDto(List<MarineCollectionDto> marineCollectionDtos, List<String> canHaeruTime) {
+        return HaeruPlaceDetailDto.builder()
+                .name(name)
+                .address(address)
+                .area(area.getAreaName())
+                .location(new Location(latitude, longitude))
+                .marineCollections(marineCollectionDtos)
+                .startTime(canHaeruTime.get(0))
+                .endTime(canHaeruTime.get(1))
                 .build();
     }
 
